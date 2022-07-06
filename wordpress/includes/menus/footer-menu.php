ca44c2cc7e42af8af634c2d661afdcd1847c3d01
Joshua <josh@hehnle.de>
2022-07-06 23:08:04 +0200

    variable initialized before the loop
    Without the initialization, some WordPress versions throw an error.

--- wordpress/includes/menus/footer-menu.php
+++ wordpress/includes/menus/footer-menu.php
@@ -2,6 +2,8 @@
 
 function footer_menu() {
     $menu_name = 'footer-menu';
+    $menu_list = '';
+
     if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
         $menu = wp_get_nav_menu_object($locations[$menu_name]);
         $menu_items = wp_get_nav_menu_items($menu->term_id);
--- wordpress/includes/menus/header-menu.php
+++ wordpress/includes/menus/header-menu.php
@@ -1,8 +1,9 @@
 <?php
 
-function header_menu()
-{
+function header_menu() {
     $menu_name = 'header-menu';
+    $menu_list = '';
+
     if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
         $menu = wp_get_nav_menu_object($locations[$menu_name]);
         $menu_items = wp_get_nav_menu_items($menu->term_id);
